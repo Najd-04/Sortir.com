c105d5190192fc40e7854867acb52f83b9849fa8
najd-04 <najdouifak@gmail.com>
2024-11-04 16:55:54 +0100

    relations

---
+++ src/Entity/Inscription.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InscriptionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+#[ORM\HasLifecycleCallbacks]
+#[ORM\Entity(repositoryClass: InscriptionRepository::class)]
+class Inscription
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateInscription = null;
+
+    #[ORM\ManyToOne(inversedBy: 'inscriptions')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Sortie $sortie = null;
+
+    #[ORM\ManyToOne(inversedBy: 'inscriptions')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Participant $participant = null;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateInscription(): ?\DateTimeInterface
+    {
+        return $this->dateInscription;
+    }
+#[ORM\PrePersist]
+    public function setDateInscription(): static
+    {
+        $this->dateInscription = new \DateTime();
+
+        return $this;
+    }
+
+    public function getSortie(): ?Sortie
+    {
+        return $this->sortie;
+    }
+
+    public function setSortie(?Sortie $sortie): static
+    {
+        $this->sortie = $sortie;
+
+        return $this;
+    }
+
+    public function getParticipant(): ?Participant
+    {
+        return $this->participant;
+    }
+
+    public function setParticipant(?Participant $participant): static
+    {
+        $this->participant = $participant;
+
+        return $this;
+    }
+}
--- src/Entity/Participant.php
+++ src/Entity/Participant.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ParticipantRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -11,98 +13,253 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
 class Participant implements UserInterface, PasswordAuthenticatedUserInterface
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column]
+  private ?int $id = null;
 
-    #[ORM\Column(length: 180)]
-    private ?string $email = null;
+  #[ORM\Column(length: 180)]
+  private ?string $email = null;
 
-    /**
-     * @var list<string> The user roles
-     */
-    #[ORM\Column]
-    private array $roles = [];
+  /**
+   * @var list<string> The user roles
+   */
+  #[ORM\Column]
+  private array $roles = [];
 
-    /**
-     * @var string The hashed password
-     */
-    #[ORM\Column]
-    private ?string $password = null;
+  /**
+   * @var string The hashed password
+   */
+  #[ORM\Column]
+  private ?string $password = null;
 
-    public function getId(): ?int
-    {
-        return $this->id;
+  #[ORM\Column(length: 30)]
+  private ?string $nom = null;
+
+  #[ORM\Column(length: 30)]
+  private ?string $prenom = null;
+
+  #[ORM\Column(length: 30)]
+  private ?string $pseudo = null;
+
+  #[ORM\Column]
+  private ?bool $actif = null;
+
+  #[ORM\Column(length: 10, nullable: true)]
+  private ?string $telephone = null;
+
+
+  /**
+   * @var Collection<int, Sortie>
+   */
+  #[ORM\OneToMany(targetEntity: Sortie::class, mappedBy: 'organisateur', orphanRemoval: true)]
+  private Collection $sortiesOrganisees;
+
+  /**
+   * @var Collection<int, Inscription>
+   */
+  #[ORM\OneToMany(targetEntity: Inscription::class, mappedBy: 'participant', orphanRemoval: true)]
+  private Collection $inscriptions;
+
+  public function __construct()
+  {
+    $this->sortiesOrganisees = new ArrayCollection();
+    $this->inscriptions = new ArrayCollection();
+  }
+
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
+
+  public function getEmail(): ?string
+  {
+    return $this->email;
+  }
+
+  public function setEmail(string $email): static
+  {
+    $this->email = $email;
+
+    return $this;
+  }
+
+  /**
+   * A visual identifier that represents this user.
+   *
+   * @see UserInterface
+   */
+  public function getUserIdentifier(): string
+  {
+    return (string)$this->email;
+  }
+
+  /**
+   * @return list<string>
+   * @see UserInterface
+   *
+   */
+  public function getRoles(): array
+  {
+    $roles = $this->roles;
+    // guarantee every user at least has ROLE_USER
+    $roles[] = 'ROLE_USER';
+
+    return array_unique($roles);
+  }
+
+  /**
+   * @param list<string> $roles
+   */
+  public function setRoles(array $roles): static
+  {
+    $this->roles = $roles;
+
+    return $this;
+  }
+
+  /**
+   * @see PasswordAuthenticatedUserInterface
+   */
+  public function getPassword(): ?string
+  {
+    return $this->password;
+  }
+
+  public function setPassword(string $password): static
+  {
+    $this->password = $password;
+
+    return $this;
+  }
+
+  /**
+   * @see UserInterface
+   */
+  public function eraseCredentials(): void
+  {
+    // If you store any temporary, sensitive data on the user, clear it here
+    // $this->plainPassword = null;
+  }
+
+  public function getNom(): ?string
+  {
+    return $this->nom;
+  }
+
+  public function setNom(string $nom): static
+  {
+    $this->nom = $nom;
+
+    return $this;
+  }
+
+  public function getPrenom(): ?string
+  {
+    return $this->prenom;
+  }
+
+  public function setPrenom(string $prenom): static
+  {
+    $this->prenom = $prenom;
+
+    return $this;
+  }
+
+  public function getPseudo(): ?string
+  {
+    return $this->pseudo;
+  }
+
+  public function setPseudo(string $pseudo): static
+  {
+    $this->pseudo = $pseudo;
+
+    return $this;
+  }
+
+
+  public function getActif(): ?bool
+  {
+    return $this->actif;
+  }
+
+  public function setActif(bool $actif): static
+  {
+    $this->actif = $actif;
+
+    return $this;
+  }
+
+  public function getTelephone(): ?string
+  {
+    return $this->telephone;
+  }
+
+  public function setTelephone(?string $telephone): static
+  {
+    $this->telephone = $telephone;
+
+    return $this;
+  }
+
+  /**
+   * @return Collection<int, Sortie>
+   */
+  public function getSortiesOrganisees(): Collection
+  {
+    return $this->sortiesOrganisees;
+  }
+
+  public function addSortiesOrganisee(Sortie $sortiesOrganisee): static
+  {
+    if (!$this->sortiesOrganisees->contains($sortiesOrganisee)) {
+      $this->sortiesOrganisees->add($sortiesOrganisee);
+      $sortiesOrganisee->setOrganisateur($this);
     }
 
-    public function getEmail(): ?string
-    {
-        return $this->email;
+    return $this;
+  }
+
+  public function removeSortiesOrganisee(Sortie $sortiesOrganisee): static
+  {
+    if ($this->sortiesOrganisees->removeElement($sortiesOrganisee)) {
+      // set the owning side to null (unless already changed)
+      if ($sortiesOrganisee->getOrganisateur() === $this) {
+        $sortiesOrganisee->setOrganisateur(null);
+      }
     }
 
-    public function setEmail(string $email): static
-    {
-        $this->email = $email;
+    return $this;
+  }
 
-        return $this;
-    }
+  /**
+   * @return Collection<int, Inscription>
+   */
+  public function getInscriptions(): Collection
+  {
+      return $this->inscriptions;
+  }
 
-    /**
-     * A visual identifier that represents this user.
-     *
-     * @see UserInterface
-     */
-    public function getUserIdentifier(): string
-    {
-        return (string) $this->email;
-    }
+  public function addInscription(Inscription $inscription): static
+  {
+      if (!$this->inscriptions->contains($inscription)) {
+          $this->inscriptions->add($inscription);
+          $inscription->setParticipant($this);
+      }
 
-    /**
-     * @see UserInterface
-     *
-     * @return list<string>
-     */
-    public function getRoles(): array
-    {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+      return $this;
+  }
 
-        return array_unique($roles);
-    }
+  public function removeInscription(Inscription $inscription): static
+  {
+      if ($this->inscriptions->removeElement($inscription)) {
+          // set the owning side to null (unless already changed)
+          if ($inscription->getParticipant() === $this) {
+              $inscription->setParticipant(null);
+          }
+      }
 
-    /**
-     * @param list<string> $roles
-     */
-    public function setRoles(array $roles): static
-    {
-        $this->roles = $roles;
-
-        return $this;
-    }
-
-    /**
-     * @see PasswordAuthenticatedUserInterface
-     */
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): static
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    /**
-     * @see UserInterface
-     */
-    public function eraseCredentials(): void
-    {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
-    }
+      return $this;
+  }
 }
