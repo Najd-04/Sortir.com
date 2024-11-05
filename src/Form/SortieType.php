07ff957d37d1c6a6a95908174fabd6eebc45ac7f
Mehdi <mehdi.rochereau@hotmail.fr>
2024-11-05 12:35:16 +0100

    sortie Mehdi

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -2,7 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\Lieu;
 use App\Entity\Sortie;
+use App\Entity\Ville;
+use App\Repository\LieuRepository;
+use App\Repository\VilleRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -33,6 +38,25 @@ class SortieType extends AbstractType
             ->add('duree')
             ->add('infosSortie', TextareaType::class, [
                 'label' => 'Description et infos',
+            ])
+            ->add('ville', EntityType::class, [
+                'class' => Ville::class,
+                'mapped' => false,
+                'label' => 'Ville',
+                'choice_label' => 'nom',
+                'placeholder' => '-- Choisissez une ville--',
+                'query_builder' =>  function (VilleRepository $villeRepository) {
+                    return  $villeRepository->createQueryBuilder('v')
+                        ->orderBy('v.nom', 'ASC');
+                }
+            ])
+            ->add('lieu', EntityType::class, [
+                'class' => Lieu::class,
+                'mapped' => false,
+                'label' => 'Lieu',
+                'choice_label' => 'nom',
+                'placeholder' => '-- Choisissez un lieu--',
+                'choices' => []
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Enregistrer',
