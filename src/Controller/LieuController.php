a1886f2681fb7d1eae1752e90d71242b29e72368
Mehdi <mehdi.rochereau@hotmail.fr>
2024-11-08 16:44:12 +0100

    sortie list fonctionnel

--- src/Controller/LieuController.php
+++ src/Controller/LieuController.php
@@ -2,43 +2,13 @@
 
 namespace App\Controller;
 
-use App\Entity\Lieu;
-use App\Entity\Ville;
 use App\Repository\LieuRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
 class LieuController extends AbstractController
 {
-//    #[Route('/lieu/ajouter', name: 'ajouter_lieu', methods: ['POST'])]
-//    public function ajouterLieu(Request $request, EntityManagerInterface $em): JsonResponse
-//    {
-//        $villeId = $request->request->get('ville_id');
-//        $lieuName = $request->request->get('lieu_name');
-//
-//        $ville = $em->getRepository(Ville::class)->find($villeId);
-//
-//        if (!$ville || !$lieuName) {
-//            return new JsonResponse(['error' => 'Ville ou nom du lieu manquant'], 400);
-//        }
-//
-//        $lieu = new Lieu();
-//        $lieu->setNom($lieuName);
-//        $lieu->setVille($ville);
-//
-//        $em->persist($lieu);
-//        $em->flush();
-//
-//        return new JsonResponse([
-//            'id' => $lieu->getId(),
-//            'name' => $lieu->getNom()
-//        ]);
-//    }
-
     #[Route('/get-lieux', name: 'get_lieux')]
     public function getLieux(Request $request, LieuRepository $lieuRepository): JsonResponse
     {
@@ -47,7 +17,6 @@ class LieuController extends AbstractController
 
         if ($villeId) {
             $lieux = $lieuRepository->findBy(['ville' => $villeId]);
-
         }
 
         $lieuxData = [];
@@ -57,7 +26,6 @@ class LieuController extends AbstractController
                 'nom' => $lieu->getNom(),
             ];
         }
-
         return new JsonResponse($lieuxData);
     }
 }
