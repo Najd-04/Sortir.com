d247da6688f438003747a56a8dd681fab128a9c5
Chloé Grosjean <113852947+Chloe-Gn@users.noreply.github.com>
2024-11-13 09:27:53 +0100

    MAJ du script bdd table etat

--- SQL/etat.sql
+++ SQL/etat.sql
@@ -1,7 +1,6 @@
-INSERT INTO sortir.etat (id, libelle) VALUES (1, 'En création');
-INSERT INTO sortir.etat (id, libelle) VALUES (2, 'Créée');
+INSERT INTO sortir.etat (id, libelle) VALUES (1, 'Brouillon');
+INSERT INTO sortir.etat (id, libelle) VALUES (2, 'Publiée');
 INSERT INTO sortir.etat (id, libelle) VALUES (3, 'Annulée');
-INSERT INTO sortir.etat (id, libelle) VALUES (4, 'Cloturée');
-INSERT INTO sortir.etat (id, libelle) VALUES (5, 'Activité en cours');
-INSERT INTO sortir.etat (id, libelle) VALUES (6, 'passée');
-INSERT INTO sortir.etat (id, libelle) VALUES (7, 'Annulée');
+INSERT INTO sortir.etat (id, libelle) VALUES (4, 'Clôturée');
+INSERT INTO sortir.etat (id, libelle) VALUES (5, 'En cours');
+
