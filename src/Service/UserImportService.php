a9f87d1f1c722c0761e3140d25cc108bfe89e8f7
najd-04 <najdouifak@gmail.com>
2024-11-14 17:00:22 +0100

    nettoyage comm

--- src/Service/UserImportService.php
+++ src/Service/UserImportService.php
@@ -21,21 +21,30 @@ class UserImportService
   public function importUsersFromFile($file): array
   {
     $handle = fopen($file->getPathname(), 'r');
-    while (($data = fgetcsv($handle, 1000, ',')) !== false) {
-      $site = $this->entityManager->getRepository(Site::class)->findOneBy(['nom' => $data[5]]);
 
+    // Lire chaque ligne du fichier CSV
+    while (($data = fgetcsv($handle, 1000, ',')) !== false) {
+      // Vérification de la longueur pour s'assurer d'avoir 7 éléments
+      if (count($data) !== 7) {
+        error_log("Ligne incomplète ou mal formatée dans le fichier CSV: " . implode(", ", $data));
+        continue; // Ignorer cette ligne
+      }
+
+      // Recherche du site par nom
+      $site = $this->entityManager->getRepository(Site::class)->findOneBy(['nom' => $data[5]]);
       if (!$site) {
         error_log("Site non trouvé pour le nom: " . $data[5]);
-        continue;
+        continue; // Ignorer cette ligne si le site n'est pas trouvé
       }
 
+      // Vérifie si un participant avec cet email existe déjà
       $existingParticipant = $this->entityManager->getRepository(Participant::class)->findOneBy(['email' => $data[3]]);
-
       if ($existingParticipant) {
         error_log("Participant avec email déjà existant: " . $data[3]);
-        continue;
+        continue; // Ignorer cette ligne si l'email est déjà utilisé
       }
 
+      // Crée un nouveau participant
       $participant = new Participant();
       $participant->setPseudo($data[0]);
       $participant->setPrenom($data[1]);
@@ -48,6 +57,7 @@ class UserImportService
       );
       $participant->setActif(true);
       $participant->setRoles(['ROLE_USER']);
+
       $this->entityManager->persist($participant);
     }
     fclose($handle);
@@ -55,5 +65,6 @@ class UserImportService
     $this->entityManager->flush();
     return ['status' => 'success', 'message' => 'Tous les utilisateurs ont été inscrits.'];
   }
+
 
 }
