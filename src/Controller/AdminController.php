ac348cee32f429e3b35fc335428f87cf8fdec12e
najd-04 <najdouifak@gmail.com>
2024-11-15 11:40:57 +0100

    fix /csvfile

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -17,6 +17,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
